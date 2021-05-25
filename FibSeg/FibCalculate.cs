31eeebeebb5ce7e5437169b2df3d0c398623e3e4
Mattea Swain <mmswain@tql.com>
2021-05-25 09:56:03 -0400

    Add project files.

---
+++ FibSeg/FibCalculate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibSeg
+{
+    class FibCalculate
+    {
+        public static List<int> GenerateSequence(int last = 200) 
+        { 
+            var fibs = new List<int>();
+            var a = 0;
+            fibs.Add(a);
+            var b = 1;
+            fibs.Add(b);
+            int c = 0;
+            while(c < last)
+            {
+                
+                c = a + b;
+                a = b;
+                b = c;
+                fibs.Add(c);
+            }
+
+            return fibs;
+        } 
+
+
+
+    }
+}
---
+++ FibSeg/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibSeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fibs = FibCalculate.GenerateSequence(200);
+            var sum = 0;
+
+            foreach(var fib in fibs)
+            {
+                sum = sum + fib;
+            }
+            var avg = sum / fibs.Count;
+
+            Console.WriteLine($"Fibs avg is {avg}");
+
+
+            
+            
+                
+            
+            
+
+
+
+
+
+
+            
+        }
+    }
+}
